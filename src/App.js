c9baff6cc764c1749ca12c4fc43c86160fd62a2b
julianxlozano <julianxlozano@gmail.com>
2021-09-11 16:35:10 -0500

    added background image

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/journal/bootstrap.css';
 import MyNav from './components/MyNav';
 
+
 const App = () => {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage:`url(/BGPreview.jpeg)`,
+    height: "1000px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"}}>
       <MyNav/>
      <h1>Shis</h1>
     </div>
