c2bd5c4b08b960cdc5ab379cfd5ec1f67dc7d8e8
julianxlozano <julianxlozano@gmail.com>
2021-09-23 22:19:50 -0500

    populated cards with home data

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -8,24 +8,21 @@ const PropertyCard = (props) =>{
     return(
         <div className="col align-item-center">
                <div className="card mb-3">
-                        <h3 className="card-header">Card header</h3>
+                        <h3 className="card-header">{props.property.name}</h3>
                         <div className="card-body">
-                            <h5 className="card-title">Special title treatment</h5>
-                            <h6 className="card-subtitle text-muted">Support card subtitle</h6>
+                            <h5 className="card-title">{props.property.location}</h5>
+                            <h6 className="card-subtitle text-muted">Available Now!</h6>
                         </div>
                        <img src="/Placeholder.jpg"/>
                         <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p className="card-text">A Nice little bio here</p>
                         </div>
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Cras justo odio</li>
-                            <li className="list-group-item">Dapibus ac facilisis in</li>
-                            <li className="list-group-item">Vestibulum at eros</li>
+                            <li className="list-group-item">Winter Price: {props.property.pricing_winter}</li>
+                            <li className="list-group-item">Spring Price: {props.property.pricing_spring}</li>
+                            <li className="list-group-item">Fall Price: {props.property.pricing_fall}</li>
+                            <li className="list-group-item">Summer Price: {props.property.pricing_summer}</li>
                         </ul>
-                        <div className="card-body">
-                            <a href="#" className="card-link">Card link</a>
-                            <a href="#" className="card-link">Another link</a>
-                        </div>
                         <div className= "btn-group">
                             <Link className="book-btn btn btn-outline-info" to="property/:id">Book Now!</Link>
                         </div>
--- src/components/PropertyCards.js
+++ src/components/PropertyCards.js
@@ -37,7 +37,7 @@ export default class PropertyCards extends React.Component{
             <div className="all-property-cards container col d-flex justify-content-center">
                       <div className="row row-cols-1 row-cols-md-3 g-4">
                           {console.log(this.state.properties)}
-                         {this.state.properties.map(property => <PropertyCard />)}
+                         {this.state.properties.map(property => <PropertyCard property={property} />)}
                       </div>
             </div>
         )
